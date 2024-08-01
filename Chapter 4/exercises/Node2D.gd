ed8e13f9940adf71a7945bc383a4980356904914
Mark "Scetch" lee <mark@scetch.dk>
2024-08-02 01:51:53 +0200

    Loops Edit assignment 1

--- Chapter 4/exercises/Node2D.gd
+++ Chapter 4/exercises/Node2D.gd
@@ -7,7 +7,12 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# Write a WHILE loop that prints out the values from 0 to 10.
+	# Write a WHILE loop that prints out the values from 0 to 10
+	var x = 0
+	while x <= 10:
+		print(x)
+		x = x + 1
+		
 	
 	# Write a WHILE loop that prints out the sum of every 
 	# other value from 10 to 20.
